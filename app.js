0253398726d9eb75761845f687ece5719ba095b6
Seungjee Lee <moondgod@gmail.com>
2022-10-19 15:10:33 +0900

    Update alert

--- app.js
+++ app.js
@@ -12,29 +12,34 @@ const onChange = (ev) => {
             console.log(json);
             Length = json.length;
             console.log(Length);
-            const link = `https://github.com/${Value}.png`;
-            const rink = `https://github.com/${Value}`;
-            let img = document.createElement('img');
-            img.src = link;
-            main.href = rink;
-            main.appendChild(img);
-            for (let i = 0; i < Length; i++) {
-                let List = document.createElement('button');
-                let Div = document.createElement('div');
-                let language = document.createElement('div');
-                let ListText = document.createTextNode( json[i].name );
-                let LanguageText = document.createTextNode(json[i].language);
-                Div.setAttribute('class', 'Div');
-                List.appendChild( ListText );
-                Div.appendChild( List );
-                language.appendChild(LanguageText);
-                Div.appendChild(language);
-                language.setAttribute('class','user__Text');
-                List.setAttribute('class', 'user__user');
-                user.appendChild(Div);
-                List.addEventListener("click", () => {
-                    location = json[i].html_url;
-                })
+            if(Length == 0){
+                alert("존재하지 않는 유저입니다");
+            }
+            else{
+                const link = `https://github.com/${Value}.png`;
+                const rink = `https://github.com/${Value}`;
+                let img = document.createElement('img');
+                img.src = link;
+                main.href = rink;
+                main.appendChild(img);
+                for (let i = 0; i < Length; i++) {
+                    let List = document.createElement('button');
+                    let Div = document.createElement('div');
+                    let language = document.createElement('div');
+                    let ListText = document.createTextNode( json[i].name );
+                    let LanguageText = document.createTextNode(json[i].language);
+                    Div.setAttribute('class', 'Div');
+                    List.appendChild( ListText );
+                    Div.appendChild( List );
+                    language.appendChild(LanguageText);
+                    Div.appendChild(language);
+                    language.setAttribute('class','user__Text');
+                    List.setAttribute('class', 'user__user');
+                    user.appendChild(Div);
+                    List.addEventListener("click", () => {
+                        location = json[i].html_url;
+                    })
+                }
             }
         })
     }
